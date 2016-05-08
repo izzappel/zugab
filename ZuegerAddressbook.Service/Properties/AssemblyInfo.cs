592fb4a7a20813fdc503607e497c53a14b482e52
Oliver Zürcher <oliver.zuercher@gmail.com>
2016-05-09 00:41:37 +0200

    test project reference updated & new service project added

---
+++ ZuegerAddressbook.Service/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZuegerAddressbook.Service")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Zuehlke Engineering")]
+[assembly: AssemblyProduct("ZuegerAddressbook.Service")]
+[assembly: AssemblyCopyright("Copyright © Zuehlke Engineering 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fadcf610-8030-4e23-be18-5b1bc3b4c4b5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ZuegerAddressbook.Service/ZuegerAddressbookServiceInstaller.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace ZuegerAddressbook.Service
+{
+    [RunInstaller(true)]
+    public class ZuegerAddressbookServiceInstaller : Installer
+    {
+        public ZuegerAddressbookServiceInstaller()
+        {
+            var service = new ServiceInstaller
+            {
+                DisplayName = "Züger Addressbook Service",
+                ServiceName = "ZuegerAddressbook.Service"
+            };
+
+            var serviceProcess = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.User
+            };
+
+            service.StartType = ServiceStartMode.Automatic;
+            service.DelayedAutoStart = true;
+
+            Installers.Add(serviceProcess);
+            Installers.Add(service);
+        }
+    }
+}

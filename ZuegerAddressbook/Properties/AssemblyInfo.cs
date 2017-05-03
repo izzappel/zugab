1bf9b7723a5166cd9578f674f683dcf98abab58f
izzappel <izzappel@users.noreply.github.com>
2017-05-03 18:10:36 +0200

    update version

--- ZuegerAddressbook/Properties/AssemblyInfo.cs
+++ ZuegerAddressbook/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("ZuegerAdressbook")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Zuehlke Technology Group")]
+[assembly: AssemblyCompany("Zueger Solutions")]
 [assembly: AssemblyProduct("ZuegerAdressbook")]
-[assembly: AssemblyCopyright("Copyright © Zuehlke Technology Group 2016")]
+[assembly: AssemblyCopyright("Copyright © Zueger Solutions 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
